104c87980e796916b46ebd0e0d22820a3b816054
raquel-1 <rocamartinezraquel@gmail.com>
2026-09-09 21:53:42 +0200

    py_mirror_matrix

--- A_EASY/py_mirror_matrix/py_mirror_matrix.py
+++ A_EASY/py_mirror_matrix/py_mirror_matrix.py
@@ -1,6 +1,5 @@
 def mirror_matrix(matrix: list[list[int]]) -> list[list[int]]:
-    copy = [m[::-1] for m in matrix]
-    return copy
+    return [row[::-1] for row in matrix]
 
 if __name__ == "__main__":
     print(mirror_matrix([[1,2,3],[4,5,6]]))
